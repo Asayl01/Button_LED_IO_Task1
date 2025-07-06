475e993a31e2a25e9491f4e0ade143ea3e4d23e9
Asayl01 <asayl3abdullah@gmail.com>
2025-07-06 12:49:37 +0300

    Add files via upload

---
+++ LEDs_Buttons.ino
@@ -0,0 +1,29 @@
+// تعيين الأرجل
+const int ledPins[]    = {2, 3, 4};
+const int buttonPins[] = {5, 6, 7};
+
+// حالات الليدات
+bool ledStates[]       = {false, false, false};
+bool buttonPressed[]   = {false, false, false};
+
+void setup() {
+  for (int i = 0; i < 3; i++) {
+    pinMode(ledPins[i], OUTPUT);
+    pinMode(buttonPins[i], INPUT_PULLUP);
+    digitalWrite(ledPins[i], LOW);
+  }
+}
+
+void loop() {
+  for (int i = 0; i < 3; i++) {
+    if (digitalRead(buttonPins[i]) == LOW) {
+      if (!buttonPressed[i]) {
+        buttonPressed[i] = true;
+        ledStates[i] = !ledStates[i];
+        digitalWrite(ledPins[i], ledStates[i] ? HIGH : LOW);
+      }
+    } else {
+      buttonPressed[i] = false;
+    }
+  }
+}
